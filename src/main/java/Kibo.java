5db163aa3cfce5ee7b6487f3cf48c798cb9199f6
lohjn <e1523228@u.nus.edu>
2026-08-21 00:26:02 +0800

    Rename chatbot to Kibo and add Level 0 greeting

---
+++ src/main/java/Kibo.java
@@ -0,0 +1,10 @@
+/**
+ * Runs the Kibo chatbot.
+ */
+public class Kibo {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Kibo.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
