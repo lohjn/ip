266192ec1e36b8b3c34e1d2a8ecb2af406de45d1
lohjn <e1523228@u.nus.edu>
2026-08-21 13:40:38 +0800

    Duke Level-1 completed

--- src/main/java/Kibo.java
+++ src/main/java/Kibo.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 /**
  * Runs the Kibo chatbot.
  */
 public class Kibo {
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " _  __ _ _           \n"
                 + "| |/ /(_) |__   ___  \n"
@@ -12,6 +16,21 @@ public class Kibo {
         System.out.print(banner);
         System.out.println("Hello! I'm Kibo. I am AI.");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(SEPARATOR);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(SEPARATOR);
+
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(SEPARATOR);
+                break;
+            }
+
+            System.out.println(" " + input);
+            System.out.println(SEPARATOR);
+        }
     }
 }
