d3933c85fa65d13aa171320ccb8708ecbdd6a0e2
lohjn <e1523228@u.nus.edu>
2026-08-21 13:14:08 +0800

    Added banner before greeting

--- src/main/java/Kibo.java
+++ src/main/java/Kibo.java
@@ -3,6 +3,13 @@
  */
 public class Kibo {
     public static void main(String[] args) {
+        String banner = " _  __ _ _           \n"
+                + "| |/ /(_) |__   ___  \n"
+                + "| ' / | | '_ \\ / _ \\\n"
+                + "| . \\ | | |_) | (_) |\n"
+                + "|_|\\_\\|_|_.__/ \\___/\n";
+
+        System.out.print(banner);
         System.out.println("Hello! I'm Kibo.");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
